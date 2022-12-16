3ce79fb683554e972d44f517f97a7583aceaf4f3
Alejandro Palmar <thepalmars1one@gmail.com>
2022-12-16 19:11:49 -0400

    Home Section almost done, Footer coming next

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
+import { RentComponent } from './renting/rent/rent.component';
 
 const routes: Routes = [
   {path:"Home", component: HomeComponent},
+  {path: "Rent", component: RentComponent},
   {path: "", component: HomeComponent, pathMatch: "full"},
   {path: "**", redirectTo: "/", pathMatch: "full"}
 ];
